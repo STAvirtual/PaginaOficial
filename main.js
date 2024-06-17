3c5757d5c2362d173a569bc099449fc7195fbcd7
Nicolas Gandini Lopez <nicolasgandinilopez@gmail.com>
2024-06-17 19:16:01 -0300

    countdown

--- main.js
+++ main.js
@@ -3,3 +3,41 @@ const menudesplegable = document.getElementById('menu-desplegable-sta')
 
 menubtn.addEventListener('click',function(){if (menudesplegable.classList.contains("menu-desplegado")){menudesplegable.classList.remove("menu-desplegado")}else{menudesplegable.classList.add("menu-desplegado")}})
 
+
+const storeOpening = new Date('jun 30, 2024 14:00:00');
+
+setCountdown(storeOpening);
+
+function setCountdown(countingDownTime) {
+
+    let now = new Date();
+
+    let timeRemaining = countingDownTime - now;
+
+    console.log(timeRemaining);
+
+    // Seconds
+    let seconds = Math.floor(timeRemaining / 1000);
+    // Minutes
+    let minutes = Math.floor(timeRemaining / (1000*60));
+    // Hours 
+    let hours = Math.floor(timeRemaining / (1000*60*60));
+    // Days
+    let days = Math.floor(timeRemaining / (1000*60*60*24));
+
+    let daysToDisplay = days;
+    let hoursToDisplay = hours - (days * 24);
+    let minutesToDisplay = minutes - ( hours * 60 );
+    let secondsToDisplay = seconds - ( minutes * 60 );
+
+    const daysEl = document.getElementById('days');
+    const hoursEl = document.getElementById('hours');
+    const minutesEl = document.getElementById('minutes');
+    const secondsEl = document.getElementById('seconds');
+
+    daysEl.textContent = daysToDisplay;
+    hoursEl.textContent = hoursToDisplay;
+    minutesEl.textContent = minutesToDisplay;
+    secondsEl.textContent = secondsToDisplay;
+
+}
