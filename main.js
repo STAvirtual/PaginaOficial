9354064dec7da02eb20744ea6490a13edf4b55e8
Nicolas Gandini Lopez <nicolasgandinilopez@gmail.com>
2024-06-30 22:53:19 -0300

    fecha 5 terminada beta

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menudesplegable = document.getElementById('menu-desplegable-sta')
 menubtn.addEventListener('click',function(){if (menudesplegable.classList.contains("menu-desplegado")){menudesplegable.classList.remove("menu-desplegado")}else{menudesplegable.classList.add("menu-desplegado")}})
 
 
-const storeOpening = new Date('jun 30, 2024 14:00:00');
+const storeOpening = new Date('jul 07, 2024 22:00:00');
 setInterval ('setCountdown(storeOpening)', 1000);
 
 function setCountdown(countingDownTime) {
