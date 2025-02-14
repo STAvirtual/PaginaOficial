a030d9ce4682ce270305f0cc1d89121ef058c245
Nicolas <nglwebdeveloper@gmail.com>
2025-02-14 00:19:32 -0300

    campeonato camionetas agregado

--- campeonatojspck.js
+++ campeonatojspck.js
@@ -7,33 +7,33 @@ menubtn.addEventListener('click',function(){if (menudesplegable.classList.contai
 importarCampeonato()
   
 function importarCampeonato() {
-      fetch('https://script.google.com/macros/s/AKfycbyXTuiZSR5W0zg8zv-wjONCheMXfX9ymCyAAuPty8NA6FWVxAjHddeAgBdTetu_49PN/exec', { cache: 'no-store' },{"method":"GET"})
+      fetch('https://script.google.com/macros/s/AKfycbyJ-AzKN-Opg92WwaJ55DVC5DAx8g-iPhPElu1EiqWnVOD3FAjbljNJhTh0Kwu6QeAF/exec', { cache: 'no-store' },{"method":"GET"})
         .then((response) => response.json())
         .then((json) => mostrarCampeonato(json))
         .then()
 }
-function mostrarCampeonato(pilotos) {
+function mostrarCampeonato(pilotos) {//
   pilotos.sort((a, b) => a.POS - b.POS)
   pilotos.forEach((piloto) => {
     mostrarPilotos(piloto);});
-  pintarcopadeoro()
+  //pintarcopadeoro()
 }
-function verposicion(piloto){
-
-  let pilotoposicion = piloto.querySelector('.posPilotoTabla')
-
-  let pilotoapintar = pilotoposicion.closest('.tablaPiloto')
-  let posicionvalor = pilotoposicion.textContent
-
-  if (posicionvalor <= 8) {
-    pilotoapintar.classList.add("copadeoroclasificando")
-}
-}
-function pintarcopadeoro(){
-    let pilotosyalistados = document.querySelectorAll(".tablaPiloto")
-    pilotosyalistados.forEach((piloto) => 
-      verposicion(piloto))
-}
+//function verposicion(piloto){//
+//  console.log(piloto)
+//  let pilotoposicion = piloto.querySelector('.posPilotoTabla')
+//  console.log(pilotoposicion)
+//  let pilotoapintar = pilotoposicion.closest('.tablaPiloto')
+//  let posicionvalor = pilotoposicion.textContent
+//  console.log(posicionvalor)
+//  if (posicionvalor <= 8) {
+//    pilotoapintar.classList.add("copadeoroclasificando")
+//}
+//}
+//function pintarcopadeoro(){
+//    let pilotosyalistados = document.querySelectorAll(".tablaPiloto")
+//    pilotosyalistados.forEach((piloto) => 
+//      verposicion(piloto))
+//}
 
 const tablaCampeonato = document.createElement("section")
   tablaCampeonato.className = "tablaPosiciones"
@@ -43,11 +43,11 @@ function mostrarPilotos(piloto) {
   const tarjetaPiloto = document.createElement("div");
   
   tarjetaPiloto.className += "tablaPiloto";
-  tarjetaPiloto.innerHTML = `<p class="posPilotoTabla">${piloto.POS}</p>
-                            <article class="imgPilotoTabla"><img src="./assets/autos2025/${piloto.AUTO}.png" alt="${piloto.PILOTO}"></article>
+  tarjetaPiloto.innerHTML = `<p class="posPilotoTabla colorpckp">${piloto.POS}</p>
+                            <article class="imgPilotoTabla"><img src="./assets/camionetas2025/${piloto.AUTO}.png" alt="${piloto.PILOTO}"></article>
                             <p class="nombrePilotoTabla">${piloto.PILOTO}</p>
                             <article class="imgPilotoTabla"><img src="./assets/equipos2025/${piloto.EQUIPO}.png" alt="${piloto.PILOTO}"></article>
-                            <p class="ptsPilotoTabla">${piloto.PUNTOS} PTS</p>
+                            <p class="ptsPilotoTabla colorpckp">${piloto.PUNTOS} PTS</p>
                             `
   tablaCampeonato.appendChild(tarjetaPiloto);
   campeonatomain.innerHTML = ''
